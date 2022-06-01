897a1c19cdd857db4bc4414af1e99cc6d8e0110f
barbeprisc <barbepriscamam@gmail.com>
2022-06-01 13:13:16 +0000

    notre projet PN

---
+++ database/migrations/2022_06_01_104138_create_utilisateurs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilisateursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->integer('telephone');
+            $table->string('email');
+            $table->string('matricule');
+            $table->string('username');
+            $table->string('mot_de_passe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+}
